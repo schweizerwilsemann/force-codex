ef200d909720094458080e39466c78c79beaab52
KhoaND <khoanguyenduc99@gmail.com>
2026-01-27 21:47:49 +0700

    feat: Implement `AuthGuard` for route protection, add `lucide-react` dependency, and clean up page imports.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.scss";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Providers from "@/components";
+import Providers from "@/app/providers";
+import AuthGuard from "@/app/guards/AuthGuard";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Providers>
-          {children}
+          <AuthGuard>
+            {children}
+          </AuthGuard>
         </Providers>
       </body>
     </html >
